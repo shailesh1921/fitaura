d7af971fc15d8886bcf496c57fec0691f710230b
shailesh.singh <singh44shailesh@gmail.com>
2026-06-08 11:14:24 +0530

    Fix service worker cache

--- sw.js
+++ sw.js
@@ -35,13 +35,34 @@ self.addEventListener('activate', e => {
 });
 
 // Fetch
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
+self.addEventListener('fetch', event => {
+
+  if (
+    !event.request.url.startsWith('http') ||
+    event.request.url.startsWith('chrome-extension://')
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
       return (
-        cached ||
-        fetch(e.request).then(response => {
-          return response;
+        response ||
+        fetch(event.request).then(networkResponse => {
+
+          if (
+            networkResponse &&
+            networkResponse.status === 200 &&
+            networkResponse.type === 'basic'
+          ) {
+            const clone = networkResponse.clone();
+
+            caches.open(CACHE).then(cache => {
+              cache.put(event.request, clone);
+            });
+          }
+
+          return networkResponse;
         })
       );
     })
